2ef2e65fdcfffabb4b1b18f68d38e86c76bab3e0
ya.zhang <ya.zhang@thinmoo.com>
2020-04-10 18:07:05 +0800

    年会抽奖

---
+++ h5Demo/lottery-master/js/member.js
@@ -0,0 +1,74 @@
+var member = [
+  {
+    "phone": "",
+    "name": "张进"
+  },
+  {
+    "phone": "",
+    "name": "叶秀婷"
+  },
+  {
+    "phone": "",
+    "name": "黄小红"
+  },
+  {
+    "phone": "",
+    "name": "钟永莲"
+  },
+  {
+    "phone": "",
+    "name": "唐登连"
+  },
+  {
+    "phone": "",
+    "name": "任运波"
+  },
+  {
+    "phone": "",
+    "name": "钱志国"
+  },
+  {
+    "phone": "",
+    "name": "李兴奉"
+  },
+  {
+    "phone": "",
+    "name": "李国仁"
+  },
+  {
+    "phone": "",
+    "name": "黄有上"
+  },
+  {
+    "phone": "",
+    "name": "雍余"
+  },
+  {
+    "phone": "",
+    "name": "唐景东"
+  },
+  {
+    "phone": "",
+    "name": "覃彩云"
+  },
+  {
+    "phone": "",
+    "name": "蒋柏玉"
+  },
+  {
+    "phone": "",
+    "name": "覃星杰"
+  },
+  {
+    "phone": "",
+    "name": "丁娟娟"
+  },
+  {
+    "phone": "",
+    "name": "张总"
+  },
+  {
+    "phone": "",
+    "name": "刘七巧"
+  }
+]
